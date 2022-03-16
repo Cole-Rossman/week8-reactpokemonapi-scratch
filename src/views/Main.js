0b916b498096cbd002e6249f7e9b4d487b16b46f
Cole Rossman <colerossman99@gmail.com>
2022-03-16 15:51:30 -0700

    state set for types and setTyples implemented in fetchdata

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import PokemonCard from '../components/PokeCard/PokeCard';
-import { fetchPokedex } from '../services/pokemon';
+import { fetchPokedex, fetchTypes } from '../services/pokemon';
 import PokeCard from '../components/PokeCard/PokeCard';
 
 export default function Main() {
   const [pokemon, setPokemon] = useState([]);
-
+  const [types, setTypes] = useState([]);
+ 
   useEffect(() => {
     const fetchData = async () => {
       const pokemonList = await fetchPokedex();
-      console.log(pokemonList);
       setPokemon(pokemonList);
+      const typesData = await fetchTypes();
+      setTypes(typesData);
     };
     fetchData();
   }, []);
